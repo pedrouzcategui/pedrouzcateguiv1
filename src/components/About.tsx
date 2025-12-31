b889b533fc54aed56716dd375de0890c6c4b3556
Pedro <pedrouzcategui0103@gmail.com>
2025-12-31 01:12:10 -0400

    fix: about text sizing

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,8 +5,8 @@ export default function About() {
     <section id="about" className="py-12 md:py-16">
       <div>
         <SectionLabel>About Me</SectionLabel>
-        <div className="lg:w-4/5">
-          <p className="text-4xl/15 lg:text-6xl/20 text-secondary">
+        <div className="text-justify md:text-left lg:w-4/5">
+          <p className="text-3xl/15 lg:text-6xl/20 text-secondary">
             I help businesses transform through software, A.I and systems
             re-design, saving them tens of thousands of dollars (and time).
           </p>
