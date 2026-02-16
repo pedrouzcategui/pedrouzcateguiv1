af86038e8c23d28f06380cc605229e4b23569799
Pedro <pedrouzcategui0103@gmail.com>
2026-02-15 21:12:21 -0400

    Adds GTM Tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import "katex/dist/katex.min.css";
 import "prismjs/themes/prism-okaidia.min.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -23,14 +25,23 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script
+        <Script
           id="theme-init"
           dangerouslySetInnerHTML={{
             __html: `(() => {try {const stored = localStorage.getItem("theme");const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;const theme = stored === "light" || stored === "dark" ? stored : (prefersDark ? "dark" : "light");document.documentElement.setAttribute("data-theme", theme);} catch (_) {}})();`,
           }}
         />
+        <GoogleTagManager gtmId="GTM-N9GLCL26" />
       </head>
       <body className={`${spaceGrotesk.className} `}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N9GLCL26"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar />
         <div className="min-h-screen h-full bg-primary">{children}</div>
         <Footer />
